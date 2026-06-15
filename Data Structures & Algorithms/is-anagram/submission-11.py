1564c75e56ff77b10aecdd382e5470de7b3749ef
Atharva Sunil Toskar <56105570+atharva49@users.noreply.github.com>
2026-06-15 15:31:01 -0400

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+
+        prevs = {}
+        prevt = {}
+        for i in range(len(s)):
+            prevs[s[i]] = prevs.get(s[i],0)+1
+            prevt[t[i]] = prevt.get(t[i],0)+1
+
+
+        return prevs==prevt
+        
