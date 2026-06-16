586063f2fd098ee9df73840718bfda011fb0adbd
Atharva Sunil Toskar <56105570+atharva49@users.noreply.github.com>
2026-06-15 23:24:22 -0400

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        prev = {}
+        for i,a in enumerate(nums):
+            diff = target-a
+            if a in prev:
+                return [prev.get(a),i]
+
+            prev[diff] = i
